e1ee6725ee6a856cc2def8b374b0c6cf089e127f
andrey1303110 <andrey1303110@gmail.com>
2021-12-31 01:33:12 +0200

    create for in for elements from DB on page

--- blog/news/views.py
+++ blog/news/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import Articles
 
 def news_home(request):
     data = {
         'title': 'News page',
+        'news': Articles.objects.all()
     }
     return render(request, 'news/news_home.html', data)
