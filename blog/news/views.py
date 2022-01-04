dafcbe01ac161cefabc2188b1022a32caae7f966
andrey1303110 <andrey1303110@gmail.com>
2022-01-05 00:54:53 +0200

    done commit

--- blog/news/views.py
+++ blog/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
@@ -18,6 +18,20 @@ class NewsDetailView(DetailView):
     context_object_name = 'article'
 
 
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/news_update.html'
+    context_object_name = 'article'
+    form_class = ArticlesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news/'
+    template_name = 'news/news_delete.html'
+    context_object_name = 'article'
+
+
 def create(request):
     error = ''
     if request.method == 'POST':
@@ -31,6 +45,7 @@ def create(request):
     form = ArticlesForm()
     data = {
         'form': form,
-        'error': error
+        'error': error,
+        'title': 'Форма по добавлению статьи'
     }
     return render(request, 'news/create.html', data)
