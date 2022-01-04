2d0496db25b44da806159f95135861ea1192512d
andrey1303110 <andrey1303110@gmail.com>
2022-01-04 23:22:28 +0200

    add form for add's new titles

---
+++ blog/news/forms.py
@@ -0,0 +1,26 @@
+from .models import Articles
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
+
+
+class ArticlesForm(ModelForm):
+    class Meta:
+        model = Articles
+        fields = ['title', 'anons', 'full_text', 'date']
+        widgets = {
+            "title": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Название'
+            }),
+            "anons": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Анонс'
+            }),
+            "full_text": Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Текст'
+            }),
+            "date": DateTimeInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Дата'
+            }),
+        }
--- blog/news/views.py
+++ blog/news/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
+
 
 def news_home(request):
     data = {
         'title': 'News page',
-        'news': Articles.objects.all()
+        'news': Articles.objects.order_by('-date')
     }
     return render(request, 'news/news_home.html', data)
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_home')
+        else:
+            error = 'Форма заполнена неверно'
+
+    form = ArticlesForm()
+    data = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'news/create.html', data)
