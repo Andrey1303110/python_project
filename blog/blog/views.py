4e8ab331bde0bba32e752a822a046e898182f3ff
andrey1303110 <andrey1303110@gmail.com>
2022-01-11 17:21:56 +0200

    fix auth

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib import messages
 
 
 def login_user(request):
+    data = {
+        'title': 'Login Page',
+        'button_text': 'sign in',
+        'form': LoginForm(),
+    }
     if request.method == 'POST':
         form = LoginForm(request.POST)
-        error: {''}
         if form.is_valid():
             cleaned_data = form.cleaned_data
             user = authenticate(username=cleaned_data['username'], password=cleaned_data['password'])
@@ -15,27 +20,29 @@ def login_user(request):
                 login(request, user)
                 return redirect('news_home')
             else:
-                error: {
-                    'Your account has disabled!'
-                }
+                messages.error(request, 'Вы ввели неверный пароль или логин!')
+                return render(request, 'registration/login.html', data)
         else:
-            error: {
-                'Invalid login!'
-            }
+            messages.error(request, 'Поля были неверно заполнены!')
+            return render(request, 'registration/login.html', data)
     else:
-        data = {
-            'title': 'Login Page',
-            'button_text': 'sign in',
-            'form': LoginForm(),
-        }
         return render(request, 'registration/login.html', data)
 
 
 def register(request):
-    form = UserCreationForm()
-    data = {
-        'title': 'Registration Page',
-        'button_text': 'registration',
-        'form': form,
-    }
-    return render(request, 'registration/register.html', data)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались!')
+            return redirect('login')
+        else:
+            form = UserCreationForm()
+            messages.error(request, 'Ошибка регистрации!')
+
+        data = {
+            'title': 'Registration Page',
+            'button_text': 'registration',
+            'form': form,
+        }
+        return render(request, 'registration/register.html', data)
