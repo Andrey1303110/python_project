4f5320f4cc3e7fc4eedb1af1a33c91aceffcb268
andrey1303110 <andrey1303110@gmail.com>
2022-01-19 11:34:07 +0200

    add column for admin

--- blog/news/admin.py
+++ blog/news/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Articles
 
 
-admin.site.register(Articles)
+class AdminArticles(admin.ModelAdmin):
+    list_display = ('title', 'full_text', 'author', 'date')
+
+
+admin.site.register(Articles, AdminArticles)
