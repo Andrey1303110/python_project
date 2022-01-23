e853a8ee90314bdf0ffd398baa3ca113da023b87
andrey1303110 <andrey1303110@gmail.com>
2022-01-23 11:03:12 +0200

    delete unusable from forms news

--- blog/news/forms.py
+++ blog/news/forms.py
@@ -5,26 +5,4 @@ from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 class ArticlesForm(ModelForm):
     class Meta:
         model = Articles
-        fields = ['title', 'anons', 'full_text', 'date']
-        widgets = {
-            "title": TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Название'
-            }),
-            "anons": TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Анонс'
-            }),
-            "full_text": Textarea(attrs={
-                'class': 'form-control',
-                'placeholder': 'Текст'
-            }),
-            "date": DateTimeInput(
-                attrs={
-                    'type': 'datetime-local',
-                    'class': 'form-control',
-                    'placeholder': 'Дата'
-                },
-                format='%m/%d/%Y %H:%M',
-            )
-        }
+        fields = ['title', 'anons', 'full_text', 'date', 'image']
