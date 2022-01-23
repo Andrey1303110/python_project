1ed14e13590838c0d91c63473772b27d8001872f
andrey1303110 <andrey1303110@gmail.com>
2022-01-23 10:48:38 +0200

    fix js

--- blog/main/static/main/js/offers.js
+++ blog/main/static/main/js/offers.js
@@ -1,8 +1,3 @@
-let first_tr = document.querySelectorAll("tbody tr td:nth-child(1)")
-for (let i = 0; i < first_tr.length; i++) {
-    first_tr[i].textContent = i+1;
-}
-
 function set_input_values() {
     let offer_input = document.querySelectorAll('#id_offer_title')
     let offer_title = document.querySelectorAll('.title_offer')
