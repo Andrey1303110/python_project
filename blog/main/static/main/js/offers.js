a5643a48e20da6631899d0c17e33e0a1b39d15dc
andrey1303110 <andrey1303110@gmail.com>
2022-01-24 18:23:44 +0200

    delete unusable

--- blog/main/static/main/js/offers.js
+++
@@ -1,9 +0,0 @@
-function set_input_values() {
-    let offer_input = document.querySelectorAll('#id_offer_title')
-    let offer_title = document.querySelectorAll('.title_offer')
-    for (let i = 0; i < offer_title.length; i++) {
-        offer_input[i].value = offer_title[i].textContent
-    }
-}
-
-document.onload = set_input_values();
